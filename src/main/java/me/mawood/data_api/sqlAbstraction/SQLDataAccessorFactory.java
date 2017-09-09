cde0b53e72900cd1474180dbd55af8d492c83803
Matthew Wood <mawoodmain@googlemail.com>
2017-09-09 17:20:33 +0100

    Moved to using config file, added runnable jar to repo

--- src/main/java/me/mawood/data_api/sqlAbstraction/SQLDataAccessorFactory.java
+++ src/main/java/me/mawood/data_api/sqlAbstraction/SQLDataAccessorFactory.java
@@ -2,7 +2,9 @@ package me.mawood.data_api.sqlAbstraction;
 
 import me.mawood.data_api.sqlAbstraction.SQLDataAccessor;
 
+import java.io.*;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * data_api
@@ -11,12 +13,37 @@ import java.sql.SQLException;
 public class SQLDataAccessorFactory
 {
     private static SQLDataAccessor sql = null;
+    private static String hostname;
+    private static int port;
+    private static String database;
+    private static String username;
+    private static String password;
+    static
+    {
+        Properties prop = new Properties();
+        try
+        {
+            InputStream input = new FileInputStream("sql.config");
+            prop.load(input);
+
+        } catch (IOException e)
+        {
+            System.err.println("Failed to open config file");
+            System.exit(-1);
+
+        }
+        hostname = prop.getProperty("hostname");
+        port = Integer.parseInt(prop.getProperty("port"));
+        database = prop.getProperty("database");
+        username = prop.getProperty("username");
+        password = prop.getProperty("password");
+    }
 
     public static SQLDataAccessor getInstance()
     {
         if(sql == null) try
         {
-            sql = new SQLDataAccessor("localhost",3306,"data2","root","P@ssw0rd");
+            sql = new SQLDataAccessor(hostname,port,database,username,password);
         } catch (ClassNotFoundException | SQLException e)
         {
             System.err.println("Could not connect to database error:");

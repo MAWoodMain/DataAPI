8248284c397a124dcdad20c55be31d0572493f9a
Matthew Wood <mawoodmain@googlemail.com>
2017-09-13 14:23:35 +0100

    Hid basic error controller on swagger-ui

--- src/main/java/me/mawood/data_api/SwaggerConfig.java
+++ src/main/java/me/mawood/data_api/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package me.mawood.data_api;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -20,8 +22,7 @@ public class SwaggerConfig {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
                 .build();
     }
 }
